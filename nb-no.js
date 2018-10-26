9b9eb8cd80d812236f311d765ac871adf9185c5f
Marianne Hval <mariannehval@gmail.com>
2018-10-26 15:23:59 +0200

    nb-no Norwegian

---
+++ nb-no.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Logg inn med e-post',
+    cadastro: 'Registrer',
+    senha: 'Passord',
+    concluir: 'Fullfør',
+    preenchaOFormularioCorretamente: 'Vennligst fyll ut skjemaet riktig!',
+
+    // FBLogin
+    acessarComOFacebook: 'Logg inn med Facebook',
+    aguarde: 'Vent litt...',
+
+    // Home
+    musicasEmDestaque: 'Utvalgte Sanger',
+    artistasEmDestaque: 'Utvalgte Artister',
+    atividadeRecente: 'Nylig Aktivitet',
+    escutou: 'hørte på',
+    curtiu: 'likte',
+    aMusica: 'sangen',
+
+    // Nav Bottom
+    home: 'Hjem',
+    buscar: 'Søk',
+    explorar: 'Utforsk',
+    colecao: 'Samling',
+    perfil: 'Profil',
+
+    // Perfil
+    nome: 'Navn',
+    email: 'E-post',
+    sair: 'Logg ut',
+    instrumentoPrincipal: 'Hovedinstrument',
+    guitarra: 'Gitar',
+    baixo: 'Bass',
+    bateria: 'Trommer',
+    voz: 'Vokal',
+    violao: 'Akustisk gutar',
+    avalieNaGooglePlay: 'Rate Appen',
+    editarPerfil: 'Rediger profil',
+    faleConosco: 'Kontakt oss',
+    comprarVersaoPremium: 'Kjøp Premium-versjon',
+    removerAnuncios: 'Fjern reklame (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Om deg',
+    faleUmPoucoSobreVoce: 'Fortell oss litt om deg selv',
+    localOndeMora: 'Sted',
+    ondeVoceMora: 'Hvor bor du?',
+    digiteSeuUsername: 'Skriv inn ditt brukernavn. Eksempel: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Skriv inn ditt WhatsApp-nummer',
+    linkParaSeuCanalNoYoutube: 'Link to din YouTube-kanal',
+    canalNoYoutube: 'YouTube-kanal',
+    esteDadoNaoPodeSerEditado: 'Denne informasjonen kan ikke bli redigert!',
+
+    // Editar
+    salvar: 'Lagre',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Vennligst forklar problemet eller spørsmålet ditt.',
+    enviar: 'Send',
+
+    // Editar perfil
+    atualizeSeusDados: 'Oppdater din informasjon',
+
+    // Coleção
+    musicasCurtidas: 'Elskede Sanger',
+    
+    // Música
+    para: 'for',
+    tonalidade: 'Key',
+    outrasMusicas: 'Andre sanger',
+
+    // Offline
+    voceEstaOffline: 'Du er offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Sjekk internettforbindelsen din og prøv igjen',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Spill dine yndlingssanger med akkompagnement!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Logg inn for å få det meste ut av appen din'
+}
+
+export default strings;
