4552eff052a6bec6e2c535fb389110a69dcb154b
Oleg Filonchuk <34554336+OlegFilonchuk@users.noreply.github.com>
2018-10-26 09:04:17 +0300

    translate to russian language

---
+++ ru-RU.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Войдите с помощью E-mail',
+    cadastro: 'Зарегистрируйтесь',
+    senha: 'Пароль',
+    concluir: 'Завершить',
+    preenchaOFormularioCorretamente: 'Пожалуйста, заполните форму верно!',
+
+    // FBLogin
+    acessarComOFacebook: 'Войти с помощью Facebook',
+    aguarde: 'Подождите минутку...',
+
+    // Home
+    musicasEmDestaque: 'Избранные Песни',
+    artistasEmDestaque: 'Избранные Исполнители',
+    atividadeRecente: 'Последние Действия',
+    escutou: 'Слушал',
+    curtiu: 'понравилось',
+    aMusica: 'песня',
+
+    // Nav Bottom
+    home: 'Дом',
+    buscar: 'Поиск',
+    explorar: 'Исследовать',
+    colecao: 'Коллекция',
+    perfil: 'Профиль',
+
+    // Perfil
+    nome: 'Имя',
+    email: 'E-mail',
+    sair: 'Выйти',
+    instrumentoPrincipal: 'Основной инструмент',
+    guitarra: 'Гитара',
+    baixo: 'Бас',
+    bateria: 'Ударные',
+    voz: 'Вокал',
+    violao: 'Акустическая гитара',
+    avalieNaGooglePlay: 'Оценить приложение',
+    editarPerfil: 'Изменить профиль',
+    faleConosco: 'Свяжитесь с нами',
+    comprarVersaoPremium: 'купить Премиум-версию',
+    removerAnuncios: 'Убрат рекламу (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'О вас',
+    faleUmPoucoSobreVoce: 'Расскажите нам немного о себе',
+    localOndeMora: 'Местонахождение',
+    ondeVoceMora: 'Где вы живете?',
+    digiteSeuUsername: 'Введите ваше имя пользователя. Например: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Введите ваш номер WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Ссылка на ваш YouTube канал',
+    canalNoYoutube: 'Канал YouTube',
+    esteDadoNaoPodeSerEditado: 'Эти данные не мгут быть изменены!',
+
+    // Editar
+    salvar: 'Сохранить',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Пожалуйста, подробно опишите вашу проблему или вопрос.',
+    enviar: 'Отправить',
+
+    // Editar perfil
+    atualizeSeusDados: 'Обновить вашу информацию',
+
+    // Coleção
+    musicasCurtidas: 'Понравившиеся Песни',
+    
+    // Música
+    para: 'для',
+    tonalidade: 'Ключ',
+    outrasMusicas: 'Другие Песни',
+
+    // Offline
+    voceEstaOffline: 'Вы не в сети!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Проверьте соединение с сетью и повторите попытку',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Играйте выши любимые песни с сопровождением!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Войдите, чтобы получить все возможности вашего приложения'
+}
+
+export default strings;
