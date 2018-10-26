9aef014a36a8e3b6b07f5f1315f6ee1493a61543
Anthony Da Mota <anthonyk.damota@gmail.com>
2018-10-26 10:49:12 +0200

    French translation

---
+++ fr-fr.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Se connecter via E-mail',
+    cadastro: 'Créer un compte',
+    senha: 'Mot de passe',
+    concluir: 'S\'inscrire',
+    preenchaOFormularioCorretamente: 'Merci de remplir tous les champs',
+
+    // FBLogin
+    acessarComOFacebook: 'Se connecter via Facebook',
+    aguarde: 'Merci de patientier...',
+
+    // Home
+    musicasEmDestaque: 'Musiques en vedette',
+    artistasEmDestaque: 'Artistes en vedette',
+    atividadeRecente: 'Activité récente',
+    escutou: 'a écouté',
+    curtiu: 'a aimé',
+    aMusica: 'la musique',
+
+    // Nav Bottom
+    home: 'Accueil',
+    buscar: 'Rechercher',
+    explorar: 'Explorer',
+    colecao: 'Collection',
+    perfil: 'Profil',
+
+    // Perfil
+    nome: 'Nom',
+    email: 'E-mail',
+    sair: 'Se déconnecter',
+    instrumentoPrincipal: 'Instrument principal',
+    guitarra: 'Guitare',
+    baixo: 'Basse',
+    bateria: 'Batterie',
+    voz: 'Voix',
+    violao: 'Guitare acoustique',
+    avalieNaGooglePlay: 'Noter l\'application',
+    editarPerfil: 'Modifier le profil',
+    faleConosco: 'Nous contacter',
+    comprarVersaoPremium: 'Acheter la version premium',
+    removerAnuncios: 'Supprimer les pubs (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'À propos de vous',
+    faleUmPoucoSobreVoce: 'Parlez-vous un peu de vous',
+    localOndeMora: 'Localisation',
+    ondeVoceMora: 'Où habitez-vous ?',
+    digiteSeuUsername: 'Votre nom d\'utilisateur. Exemple: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Votre numéro WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Lien vers votre chaîne YouTube',
+    canalNoYoutube: 'Chaîne YouTube',
+    esteDadoNaoPodeSerEditado: 'Cette donnée ne peut être modifiée !',
+
+    // Editar
+    salvar: 'Sauvegarder',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Veuillez préciser votre problème ou question.',
+    enviar: 'Envoyer',
+
+    // Editar perfil
+    atualizeSeusDados: 'Mise à jour de vos informations',
+
+    // Coleção
+    musicasCurtidas: 'Musiques aimées',
+    
+    // Música
+    para: 'pour',
+    tonalidade: 'Clé',
+    outrasMusicas: 'Autres musiques',
+
+    // Offline
+    voceEstaOffline: 'Vous n\'êtes pas en ligne !',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Vérifiez votre connexion Internet puis réessayez',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Jouez vos morceaux favoris avec accompagnement !',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Connectez-vous pour profiter de toute l\'application'
+}
+
+export default strings;
