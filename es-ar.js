5224ed80d75d767134e9362d9834df7cc7db07c6
Andrew C <andrealaurapesci@gmail.com>
2018-10-26 14:27:15 -0300

    Add Spanish (AR) translations

---
+++ es-ar.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Ingresar con correo electrónico',
+    cadastro: 'Registrarse',
+    senha: 'Contraseña',
+    concluir: 'Terminar',
+    preenchaOFormularioCorretamente: 'Complete el formulario correctamente por favor!',
+
+    // FBLogin
+    acessarComOFacebook: 'Ingresar con Facebook',
+    aguarde: 'Espere un momento...',
+
+    // Home
+    musicasEmDestaque: 'Canciones destacadas',
+    artistasEmDestaque: 'Artistas destacados',
+    atividadeRecente: 'Actividad Reciente',
+    escutou: 'escuchado a',
+    curtiu: 'favorito',
+    aMusica: 'la canción',
+
+    // Nav Bottom
+    home: 'Inicio',
+    buscar: 'Buscar',
+    explorar: 'Explorar',
+    colecao: 'Colleción',
+    perfil: 'Perfil',
+
+    // Perfil
+    nome: 'Nombre',
+    email: 'Correo electrónico',
+    sair: 'Salir',
+    instrumentoPrincipal: 'Instrumento principal',
+    guitarra: 'Guitarra',
+    baixo: 'Bajo',
+    bateria: 'Batería',
+    voz: 'Vocal',
+    violao: 'Guitarra acústica',
+    avalieNaGooglePlay: 'Reseña',
+    editarPerfil: 'Editar perfil',
+    faleConosco: 'Contacto',
+    comprarVersaoPremium: 'Comprar versión Premium',
+    removerAnuncios: 'Quitar ads (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Acerca de vos',
+    faleUmPoucoSobreVoce: 'Cuéntanos un poco más sobre vos',
+    localOndeMora: 'Licalización',
+    ondeVoceMora: '¿Dónde vives?',
+    digiteSeuUsername: 'Ingrese su nombre de usuario. Ejemplo: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Ingrese su número de WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Enlace a tu canal YouTube',
+    canalNoYoutube: 'Canal YouTube',
+    esteDadoNaoPodeSerEditado: '¡Este dato no puede ser editado!',
+
+    // Editar
+    salvar: 'Guardar',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Please explain in detail your problem or question.',
+    enviar: 'Enviar',
+
+    // Editar perfil
+    atualizeSeusDados: 'Actualize sus datos',
+
+    // Coleção
+    musicasCurtidas: 'Canciones Favoritas',
+    
+    // Música
+    para: 'para',
+    tonalidade: 'Nota',
+    outrasMusicas: 'Otras canciones',
+
+    // Offline
+    voceEstaOffline: '¡Estas fuera de línea!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Verifique su conneción a internet e intente de nuevo',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: '¡Reproduce tus canciones favoritas con acompañamiento!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Ingrese para obetener lo mejor de tu aplicación'
+}
+
+export default strings;
