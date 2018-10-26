0d5f93f7b800e570664329fab407c51362f24458
Thorsten Frommen <info@tfrommen.de>
2018-10-26 09:28:10 +0200

    Add German translation. #1

---
+++ de-de.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Mit E-Mail-Adresse anmelden',
+    cadastro: 'Registrieren',
+    senha: 'Password',
+    concluir: 'Beenden',
+    preenchaOFormularioCorretamente: 'Bitte fülle das Formular aus!',
+
+    // FBLogin
+    acessarComOFacebook: 'Mit Facebook anmelden',
+    aguarde: 'Einen Moment bitte...',
+
+    // Home
+    musicasEmDestaque: 'Ausgewählte Lieder',
+    artistasEmDestaque: 'Ausgewählte Künstler',
+    atividadeRecente: 'Letzte Aktivität',
+    escutou: 'gehört',
+    curtiu: 'geliket',
+    aMusica: 'das Lied',
+
+    // Nav Bottom
+    home: 'Home',
+    buscar: 'Suche',
+    explorar: 'Entdecken',
+    colecao: 'Sammlung',
+    perfil: 'Profil',
+
+    // Perfil
+    nome: 'Name',
+    email: 'E-Mail-Adresse',
+    sair: 'Abmelden',
+    instrumentoPrincipal: 'Hauptinstrument',
+    guitarra: 'Gitarre',
+    baixo: 'Bass',
+    bateria: 'Schalgzeug',
+    voz: 'Gesang',
+    violao: 'Akustikgitarre',
+    avalieNaGooglePlay: 'App Bewerten',
+    editarPerfil: 'Profil bearbeiten',
+    faleConosco: 'Kontakt',
+    comprarVersaoPremium: 'Premium-Version kaufen',
+    removerAnuncios: 'Werbung entfernen (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Über dich',
+    faleUmPoucoSobreVoce: 'Erzähle uns ein bisschen über dich',
+    localOndeMora: 'Ort',
+    ondeVoceMora: 'Wo wohnst du?',
+    digiteSeuUsername: 'Gib deinen Benutzernamen ein. Beispiel: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Gib deine WhatsApp-Nummer ein',
+    linkParaSeuCanalNoYoutube: 'Link zu deinem YouTube-Kanal',
+    canalNoYoutube: 'YouTube-Kanal',
+    esteDadoNaoPodeSerEditado: 'Das kannst du nicht bearbeiten!',
+
+    // Editar
+    salvar: 'Speichern',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Erzähle uns von deinem Problem oder deiner Frage.',
+    enviar: 'Senden',
+
+    // Editar perfil
+    atualizeSeusDados: 'Informationen aktualisieren',
+
+    // Coleção
+    musicasCurtidas: 'Lieblingslieder',
+    
+    // Música
+    para: 'für',
+    tonalidade: 'Tonart',
+    outrasMusicas: 'Andere Lieder',
+
+    // Offline
+    voceEstaOffline: 'Du bist offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Bitte überprüfe deine Internetverbindung, und versuche es erneut',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Spiele deine Lieblingslieder mit Begleitung!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Los, anmelden!'
+}
+
+export default strings;
