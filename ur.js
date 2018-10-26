958a19326c8e24fcb3195ff8a85f6f34049fa71d
imran shad <imran.shad@ymail.com>
2018-10-26 01:58:52 -0400

    Urdu translated file added

---
+++ ur.js
@@ -0,0 +1,84 @@
+let strings = {
+
+
+	//  رجسٹریشن  
+    ای میل لکھیں: 'Sign in with E-mail',
+     رجسٹریشن: 'Register',
+    پاس ورڈ: 'Password',
+    اختتام کریں: 'Finish',
+    فارم درست طریقے سے بھریں: 'Please fill out the form correctly!',
+
+    // فیس بک لاگ ان	
+    فیس بک کے ساتھ سائن ان کریں: 'Sign in with Facebook',
+    انتظار کرو: 'Wait a moment...',
+
+    																	// Home
+    نمایاں گانے: 'Featured Songs',
+    نمایاں فنکار: 'Featured Artists',
+    حالیہ سرگرمی: 'Recent Activity',
+    سنیے: 'listened to',
+    پسندیدھ: 'liked',
+       گانا: 'the song',
+
+    																// Nav Bottom
+    ھوم: 'Home',
+    تلاش کریں: 'Search',
+    ڈھونڈیں: 'Explore',
+    مجموعہ: 'Collection',
+    پروفائل: 'Profile',
+
+   																 // Profile
+    نام: 'Name',
+    ای میل: 'E-mail',
+        لاگ آوٹ: 'Log out',   
+    مین آلہ: 'Main instrument',
+    گٹار: 'Guitar',
+    باس: 'Bass',
+    ڈرم: 'Drums',
+    ووکل: 'Vocal',
+    صوتی گٹار: 'Acoustic guitar',
+    اپلیکیشن کوالٹی: 'Rate App',
+    پروفائل میں ترمیم کریں: 'Edit profile',
+    ہم سے رابطہ کریں: 'Contact us',
+    پریمیم ورژن خریدیں: 'Buy Premium version',
+    اشتھارات کو ہٹا دیں (پریمیم): 'Remove ads (Pro)',
+
+    //Meus Dados
+    تمھارے بارے میں: 'About you',
+    ہمیں اپنے بارے میں تھوڑا بتاو: 'Tell us a little about yourself',
+    مقام: 'Location',
+    آپ کہاں رہتے ہو: 'Where do you live?',
+    اپنے یوزر نام کا اندراج کرو. مثال:@backingtracksapp: 'Enter your username. Example: @backingtracksapp',
+    اپنے WhatsApp نمبر درج کریں: 'Enter your WhatsApp number',
+    اپنے YouTube چینل سے لنک کریں.: 'Link to your YouTube channel',
+    YouTube چینل: 'YouTube channel',
+    یہ ڈیٹا ترمیم نہیں کیا جا سکتا: 'This data can not be edited!',
+
+    // Editar
+    محفوظ کریں: 'Save',
+
+    // Fale conosco
+    براہ کرم اپنی دشواری یا سوال کے بارے میں وضاحت کریں: 'Please explain in detail your problem or question.',
+    بھیجیں: 'Send',
+
+    // Editar perfil
+    اپنی معلومات کو اپ ڈیٹ کریں: 'Update your info',
+
+    // Coleção
+    پسندیدہ گانے: 'Loved Songs',
+    
+    // Música
+    کے لئے: 'for',
+    کلیدی: 'Key',
+    دوسرے گانے: 'Other songs',
+
+    // Offline
+    آپ آف لائن ہیں: 'You are offline!',
+    اپنا انٹرنیٹ کنکشن چیک کریں اور دوبارہ کوشش کریں: 'Check your internet connection and try again',
+
+    // Login
+    ساتھ ساتھ اپنے پسندیدہ گانا کھیلیں: 'Play your favorite songs with accompaniment!',
+    اپنے ایپ سے زیادہ تر حاصل کرنے کیلئے سائن ان کریں: 'Sign in to get the most out of your app'
+}
+
+export default strings;
