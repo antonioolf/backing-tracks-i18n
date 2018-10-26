7e758dec6a6ea9344456d97cfe3d31269ebc0d0c
Oleg Filonchuk <34554336+OlegFilonchuk@users.noreply.github.com>
2018-10-26 09:21:16 +0300

    translate to ukrainian language

---
+++ uk-UA.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Увійти за допомогою E-mail',
+    cadastro: 'Зареєструватись',
+    senha: 'Пароль',
+    concluir: 'Закінчити',
+    preenchaOFormularioCorretamente: 'Заповніть форму вірно, будь ласка!',
+
+    // FBLogin
+    acessarComOFacebook: 'Увійти за допомогою Facebook',
+    aguarde: 'Зачекайте хвилинку...',
+
+    // Home
+    musicasEmDestaque: 'Обрані Пісні',
+    artistasEmDestaque: 'Обрані Виконавці',
+    atividadeRecente: 'Попередня активність',
+    escutou: 'слухав',
+    curtiu: 'вподобав',
+    aMusica: 'пісня',
+
+    // Nav Bottom
+    home: 'Дім',
+    buscar: 'Пошук',
+    explorar: 'Дослідження',
+    colecao: 'Колекція',
+    perfil: 'Профіль',
+
+    // Perfil
+    nome: 'Найменування',
+    email: 'E-mail',
+    sair: 'Вийти',
+    instrumentoPrincipal: 'Основний інструмент',
+    guitarra: 'Гітара',
+    baixo: 'Бас',
+    bateria: 'Барабани',
+    voz: 'Вокал',
+    violao: 'Акустична гітара',
+    avalieNaGooglePlay: 'Оцінити Додаток',
+    editarPerfil: 'Редагувати профіль',
+    faleConosco: 'Напишіть нам',
+    comprarVersaoPremium: 'Купити преміум-версію',
+    removerAnuncios: 'Прибрати рекламу (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Про вас',
+    faleUmPoucoSobreVoce: 'Розкажіть нам про себе',
+    localOndeMora: 'Положення',
+    ondeVoceMora: 'Де ви живете?',
+    digiteSeuUsername: 'Введіть вашу назву користувача. Наприклад: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Введіть ваш номер WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Посилання на ваш канал YouTube',
+    canalNoYoutube: 'Канал YouTube',
+    esteDadoNaoPodeSerEditado: 'Ці дані не можуть бути змінені!',
+
+    // Editar
+    salvar: 'Зберегти',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Опишіть детально вашу проблему або питання.',
+    enviar: 'Відправити',
+
+    // Editar perfil
+    atualizeSeusDados: 'Оновити вашу інформацію',
+
+    // Coleção
+    musicasCurtidas: 'Улюблені пісні',
+    
+    // Música
+    para: 'для',
+    tonalidade: 'Ключ',
+    outrasMusicas: 'Інші пісні',
+
+    // Offline
+    voceEstaOffline: 'Ви не в мережі!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Перевірте доступ до мережі та спробуйте ще',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Грайте ваі улюблені пісні з акомпанементом!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Увійдіть для отримання усіх можливостей додатку'
+}
+
+export default strings;
