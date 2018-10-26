311b3b93f8d87ae9c382bb31fc5e2070f01cdf9c
Evaldas Stanelis <evaldas@stanelis.lt>
2018-10-26 11:17:17 +0300

    Add lithuanian translation

---
+++ lt-LT.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Prisijungti naudojant El. paštą',
+    cadastro: 'Prisiregistruoti',
+    senha: 'Slaptažodis',
+    concluir: 'Pabaiga',
+    preenchaOFormularioCorretamente: 'Prašome teisingai užpildyti formą!',
+
+    // FBLogin
+    acessarComOFacebook: 'Prisijungti naudojant Facebook',
+    aguarde: 'Palaukite minutę...',
+
+    // Home
+    musicasEmDestaque: 'Charakterizuotos Dainos',
+    artistasEmDestaque: 'Charakterizuoti Atlikėjai',
+    atividadeRecente: 'Paskutiniai veiksmai',
+    escutou: 'klausyta',
+    curtiu: 'patiko',
+    aMusica: 'daina',
+
+    // Nav Bottom
+    home: 'Namai',
+    buscar: 'Paieška',
+    explorar: 'Naršyti',
+    colecao: 'Kolekcija',
+    perfil: 'Profilis',
+
+    // Perfil
+    nome: 'Vardas',
+    email: 'El. paštas',
+    sair: 'Atsijungti',
+    instrumentoPrincipal: 'Pagrindinis instrumentas',
+    guitarra: 'Gitara',
+    baixo: 'Bosinė gitara',
+    bateria: 'Bugnai',
+    voz: 'Vokalas',
+    violao: 'Akustinė gitara',
+    avalieNaGooglePlay: 'Įvertinkite programėlę',
+    editarPerfil: 'Redaguoti profilį',
+    faleConosco: 'Susisiekite su mumis',
+    comprarVersaoPremium: 'Įsigyti geresnę versiją',
+    removerAnuncios: 'Panaikinti reklamas (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Apie jus',
+    faleUmPoucoSobreVoce: 'Papasakokit truputį apie save',
+    localOndeMora: 'Vieta',
+    ondeVoceMora: 'Kur jūs gyvenate?',
+    digiteSeuUsername: 'Įveskite vartotojo vardą. Pavyzdžiui: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Įveskite jūsų WhatsApp numerį',
+    linkParaSeuCanalNoYoutube: 'Nuoroda į jūsų YouTube kanalą',
+    canalNoYoutube: 'YouTube kanalas',
+    esteDadoNaoPodeSerEditado: 'Šie duomenys nėra redaguojami!',
+
+    // Editar
+    salvar: 'Išsaugoti',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Prašome detaliai paaiškinti jūsų problemą.',
+    enviar: 'Išsiųsti',
+
+    // Editar perfil
+    atualizeSeusDados: 'Atnaujinti jūsų informaciją',
+
+    // Coleção
+    musicasCurtidas: 'Patikusios dainos',
+    
+    // Música
+    para: 'už',
+    tonalidade: 'Raktas',
+    outrasMusicas: 'Kitos dainos',
+
+    // Offline
+    voceEstaOffline: 'Jūs esate atsijungę!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Patikrinkite interneto prisijungimą ir bandykite dar kartą',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Grokite savo mėgstamas dainas su pritarimu!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Prisijunkite, kad išnaudotumėt visas galimybes'
+}
+
+export default strings;
