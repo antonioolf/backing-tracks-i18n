d3c33069b012d103df686f5357cf310f572173ac
Antônio Oliveira <antonio.filho128@gmail.com>
2018-10-26 01:13:58 -0300

    Adding current translations

---
+++ en-us.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Sign in with E-mail',
+    cadastro: 'Register',
+    senha: 'Password',
+    concluir: 'Finish',
+    preenchaOFormularioCorretamente: 'Please fill out the form correctly!',
+
+    // FBLogin
+    acessarComOFacebook: 'Sign in with Facebook',
+    aguarde: 'Wait a moment...',
+
+    // Home
+    musicasEmDestaque: 'Featured Songs',
+    artistasEmDestaque: 'Featured Artists',
+    atividadeRecente: 'Recent Activity',
+    escutou: 'listened to',
+    curtiu: 'liked',
+    aMusica: 'the song',
+
+    // Nav Bottom
+    home: 'Home',
+    buscar: 'Search',
+    explorar: 'Explore',
+    colecao: 'Collection',
+    perfil: 'Profile',
+
+    // Perfil
+    nome: 'Name',
+    email: 'E-mail',
+    sair: 'Log out',
+    instrumentoPrincipal: 'Main instrument',
+    guitarra: 'Guitar',
+    baixo: 'Bass',
+    bateria: 'Drums',
+    voz: 'Vocal',
+    violao: 'Acoustic guitar',
+    avalieNaGooglePlay: 'Rate App',
+    editarPerfil: 'Edit profile',
+    faleConosco: 'Contact us',
+    comprarVersaoPremium: 'Buy Premium version',
+    removerAnuncios: 'Remove ads (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'About you',
+    faleUmPoucoSobreVoce: 'Tell us a little about yourself',
+    localOndeMora: 'Location',
+    ondeVoceMora: 'Where do you live?',
+    digiteSeuUsername: 'Enter your username. Example: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Enter your WhatsApp number',
+    linkParaSeuCanalNoYoutube: 'Link to your YouTube channel',
+    canalNoYoutube: 'YouTube channel',
+    esteDadoNaoPodeSerEditado: 'This data can not be edited!',
+
+    // Editar
+    salvar: 'Save',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Please explain in detail your problem or question.',
+    enviar: 'Send',
+
+    // Editar perfil
+    atualizeSeusDados: 'Update your info',
+
+    // Coleção
+    musicasCurtidas: 'Loved Songs',
+    
+    // Música
+    para: 'for',
+    tonalidade: 'Key',
+    outrasMusicas: 'Other songs',
+
+    // Offline
+    voceEstaOffline: 'You are offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Check your internet connection and try again',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Play your favorite songs with accompaniment!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Sign in to get the most out of your app'
+}
+
+export default strings;
---
+++ pt-br.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Entrar com E-mail',
+    cadastro: 'Cadastro',
+    senha: 'Senha',
+    concluir: 'Concluir',
+    preenchaOFormularioCorretamente: 'Preencha o formulário corretamente!',
+
+    // FBLogin
+    acessarComOFacebook: 'Acessar com o Facebook',
+    aguarde: 'Aguarde...',
+    
+    // Home
+    musicasEmDestaque: 'Músicas em destaque',
+    artistasEmDestaque: 'Artistas em destaque',
+    atividadeRecente: 'Atividade recente',
+    escutou: 'escutou',
+    curtiu: 'curtiu',
+    aMusica: 'a música',
+
+    // Nav Bottom
+    home: 'Home',
+    buscar: 'Buscar',
+    explorar: 'Explorar',
+    colecao: 'Coleção',
+    perfil: 'Perfil',
+
+    // Perfil
+    nome: 'Nome',
+    email: 'E-mail',
+    sair: 'Sair',
+    instrumentoPrincipal: 'Instrumento principal',
+    guitarra: 'Guitarra',
+    baixo: 'Baixo',
+    bateria: 'Bateria',
+    voz: 'Voz',
+    violao: 'Violão',
+    avalieNaGooglePlay: 'Avalie-nos na Google Play',
+    editarPerfil: 'Editar perfil',
+    faleConosco: 'Fale conosco',
+    comprarVersaoPremium: 'Comprar versão Premium',
+    removerAnuncios: 'Remover anúncios (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Sobre você',
+    faleUmPoucoSobreVoce: 'Fale um pouco sobre você',
+    localOndeMora: 'Local onde mora',
+    ondeVoceMora: 'Onde você mora?',
+    digiteSeuUsername: 'Digite seu username. Exemplo: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Digite seu número de WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Link para seu canal no YouTube',
+    canalNoYoutube: 'Canal no YouTube',
+    esteDadoNaoPodeSerEditado: 'Este dado não pode ser editado!',
+
+    // Editar
+    salvar: 'Salvar',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Por favor, explique detalhadamente o seu problema ou dúvida.',
+    enviar: 'Enviar',
+
+    // Editar perfil
+    atualizeSeusDados: 'Atualize seus dados',
+
+    // Coleção
+    musicasCurtidas: 'Músicas curtidas',
+
+    // Música
+    para: 'p/',
+    tonalidade: 'Tonalidade',
+    outrasMusicas: 'Outras músicas',
+
+    // Offline
+    voceEstaOffline: 'Você está offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Verifique a sua conexão com a internet e tente novamente',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Toque suas músicas favoritas com acompanhamento!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Faça login para aproveitar o máximo do aplicativo'
+}
+
+export default strings;
