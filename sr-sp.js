d524bf39b47d04b33484c971d11165b4468499e1
Nikola Vrhovac <nikola.vrhovac@gowi.rs>
2018-10-26 20:07:53 +0200

    Add Serbian (SR) translations

---
+++ sr-sp.js
@@ -0,0 +1,83 @@
+let strings = {
+
+  // Cadastro
+  entrarComEmail: 'Prijavite se sa e-mail adresom',
+  cadastro: 'Registrujte se',
+  senha: 'Šifra',
+  concluir: 'Završite',
+  preenchaOFormularioCorretamente: 'Upišite ime pravilno!',
+
+  // FBLogin
+  acessarComOFacebook: 'Prijavite se sa Facebook-om',
+  aguarde: 'Sačekajte...',
+
+  // Home
+  musicasEmDestaque: 'Odabrane Pesme',
+  artistasEmDestaque: 'Odabrani Izvođači',
+  atividadeRecente: 'Skorašnja Aktivnost',
+  escutou: 'slušano',
+  curtiu: 'lajkovano',
+  aMusica: 'pesma',
+
+  // Nav Bottom
+  home: 'Početna',
+  buscar: 'Pretraga',
+  explorar: 'Istražite',
+  colecao: 'Kolekcija',
+  perfil: 'Profil',
+
+  // Perfil
+  nome: 'Ime',
+  email: 'e-mail',
+  sair: 'Izlogujte se',
+  instrumentoPrincipal: 'Glavni instrument',
+  guitarra: 'Gitara',
+  baixo: 'Bas',
+  bateria: 'Bubnjevi',
+  voz: 'Vokal',
+  violao: 'Akustična gitara',
+  avalieNaGooglePlay: 'Ocenite aplikaciju',
+  editarPerfil: 'Izmenite profil',
+  faleConosco: 'Kontaktirajte nas',
+  comprarVersaoPremium: 'Kupite premium verziju',
+  removerAnuncios: 'Uklonite reklame (Pro)',
+
+  //Meus Dados
+  sobreVoce: 'O vama',
+  faleUmPoucoSobreVoce: 'Napišite nam nešto o sebi',
+  localOndeMora: 'Lokacija',
+  ondeVoceMora: 'Gde živite?',
+  digiteSeuUsername: 'Unesite vaš nadimak. Example: @backingtracksapp',
+  digiteSeuNumeroDeWhatsapp: 'Unesite svoj WhatsApp broj',
+  linkParaSeuCanalNoYoutube: 'Link ka vašem YouTube kanalu',
+  canalNoYoutube: 'YouTube kanal',
+  esteDadoNaoPodeSerEditado: 'Ovi podaci ne mogu biti izmenjeni!',
+
+  // Editar
+  salvar: 'Sačuvajte',
+
+  // Fale conosco
+  porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Molimo vas da detaljno opišete vaš problem ili pitanje.',
+  enviar: 'Pošaljite',
+
+  // Editar perfil
+  atualizeSeusDados: 'Ažurirajte informacije o sebi',
+
+  // Coleção
+  musicasCurtidas: 'Omiljene pesme',
+  
+  // Música
+  para: 'za',
+  tonalidade: 'Ton',
+  outrasMusicas: 'Druge pesme',
+
+  // Offline
+  voceEstaOffline: 'Izlogovani ste!',
+  verifiqueASuaConexaoComAInternetETenteNovamente: 'Proverite svoju internet konekciju i pokušajte ponovo',
+
+  // Login
+  toqueSuasMusicasFavoritasComAcompanhamento: 'Pustite vaše omiljene pesme sa pratnjom!',
+  facaLoginParaAproveitarOMaximoDoAplicativo: 'Ulogujte se kako biste dobili sve opcije'
+}
+
+export default strings;
