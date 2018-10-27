dbecbda64f154631e76c1a878ccc958193631f3e
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2018-10-27 00:20:02 -0700

    Mexican Spanish traslation added

---
+++ es-mx.js
@@ -0,0 +1,83 @@
+let strings = {
+
+  // Cadastro
+  entrarComEmail: 'Registrarse con Correo',
+  cadastro: 'Registro',
+  senha: 'Contraseña',
+  concluir: 'Terminar',
+  preenchaOFormularioCorretamente: '¡Por favor rellena el formulario de manera correcta!',
+
+  // FBLogin
+  acessarComOFacebook: 'Registrarse con Facebook',
+  aguarde: 'Espera...',
+
+  // Home
+  musicasEmDestaque: 'Canciones Destacadas',
+  artistasEmDestaque: 'Artistas Destacados',
+  atividadeRecente: 'Actividad Reciente',
+  escutou: 'has escuchado',
+  curtiu: 'gustos',
+  aMusica: 'la canción',
+
+  // Nav Bottom
+  home: 'Inicio',
+  buscar: 'Buscar',
+  explorar: 'Explorar',
+  colecao: 'Colección',
+  perfil: 'Perfil',
+
+  // Perfil
+  nome: 'Nombre',
+  email: 'Correo',
+  sair: 'Salir',
+  instrumentoPrincipal: 'Instrumento Principal',
+  guitarra: 'Guitarra',
+  baixo: 'Bajo',
+  bateria: 'Batería',
+  voz: 'Voz',
+  violao: 'Guitarra Acústica',
+  avalieNaGooglePlay: 'Evlúanos en Google Play',
+  editarPerfil: 'Editar Perfil',
+  faleConosco: 'Contáctanos',
+  comprarVersaoPremium: 'Comprar versión premium',
+  removerAnuncios: 'Eliminar anuncios (Pro)',
+
+  //Meus Dados
+  sobreVoce: 'Acerca de ti',
+  faleUmPoucoSobreVoce: 'Cuéntanos un poco sobre ti',
+  localOndeMora: 'Ubicación',
+  ondeVoceMora: '¿Dónde vives?',
+  digiteSeuUsername: 'Ingresa tu nombre de usuario. Por ejemplo: @backingtracksapp',
+  digiteSeuNumeroDeWhatsapp: 'Ingresa tu número de WhatsApp',
+  linkParaSeuCanalNoYoutube: 'Enlace a tu canal de YouTube',
+  canalNoYoutube: 'Canal de YouTube',
+  esteDadoNaoPodeSerEditado: '¡No puedes editar estos datos!',
+
+  // Editar
+  salvar: 'Guardar',
+
+  // Fale conosco
+  porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Por favor explicanos a detalle tu problema o pregunta.',
+  enviar: 'Enviar',
+
+  // Editar perfil
+  atualizeSeusDados: 'Actualiza tu información',
+
+  // Coleção
+  musicasCurtidas: 'Canciones Favoritas',
+  
+  // Música
+  para: 'Para',
+  tonalidade: 'Tono',
+  outrasMusicas: 'Otras Canciones',
+
+  // Offline
+  voceEstaOffline: '¡Estás desconectado!',
+  verifiqueASuaConexaoComAInternetETenteNovamente: 'Revisa tu conexión a internet e inténtalo nuevamente',
+
+  // Login
+  toqueSuasMusicasFavoritasComAcompanhamento: '¡Toca tus canciones favoritas acompañado!',
+  facaLoginParaAproveitarOMaximoDoAplicativo: 'Registrate para aprovechar al máximo la aplicación'
+}
+
+export default strings;
