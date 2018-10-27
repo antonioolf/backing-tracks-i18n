05a90939ed8b362a29d8bfc95dd1260188f612d0
Julia Lin <linj2015@users.noreply.github.com>
2018-10-26 22:39:40 -0700

    Create zh-CN.js

---
+++ zh-CN.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: '使用邮箱登录',
+    cadastro: '注册',
+    senha: '密码',
+    concluir: '完成',
+    preenchaOFormularioCorretamente: '请正确填写',
+
+    // FBLogin
+    acessarComOFacebook: '使用Facebook登录',
+    aguarde: '请稍候...',
+
+    // Home
+    musicasEmDestaque: '精选歌曲',
+    artistasEmDestaque: '精选',
+    atividadeRecente: '最新动态',
+    escutou: '最近播放',
+    curtiu: '已赞',
+    aMusica: '歌曲',
+
+    // Nav Bottom
+    home: '首页',
+    buscar: '搜索',
+    explorar: '发现',
+    colecao: '收藏',
+    perfil: '个人主页',
+
+    // Perfil
+    nome: '姓名',
+    email: '邮箱',
+    sair: '退出账号',
+    instrumentoPrincipal: '主要乐器',
+    guitarra: '吉他',
+    baixo: '贝斯',
+    bateria: '鼓',
+    voz: '歌唱',
+    violao: '民谣吉他',
+    avalieNaGooglePlay: '给个评分',
+    editarPerfil: '修改个人主页',
+    faleConosco: '联系我们',
+    comprarVersaoPremium: '购买Premium',
+    removerAnuncios: '移除广告(Pro)',
+
+    //Meus Dados
+    sobreVoce: '关于你',
+    faleUmPoucoSobreVoce: '来聊聊你自己吧',
+    localOndeMora: '位置',
+    ondeVoceMora: '居住地点',
+    digiteSeuUsername: '请输入你的用户名 比如: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: '请输入你的WhatsApp号',
+    linkParaSeuCanalNoYoutube: '你的YouTube频道连接',
+    canalNoYoutube: 'YouTube频道',
+    esteDadoNaoPodeSerEditado: '此栏位无法修改',
+
+    // Editar
+    salvar: '已存',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: '请详细叙述你遇到的问题',
+    enviar: '已送出',
+
+    // Editar perfil
+    atualizeSeusDados: '更新你的信息',
+
+    // Coleção
+    musicasCurtidas: '已点赞的歌曲',
+    
+    // Música
+    para: '为',
+    tonalidade: '调',
+    outrasMusicas: '其他歌曲',
+
+    // Offline
+    voceEstaOffline: '目前为离线状态',
+    verifiqueASuaConexaoComAInternetETenteNovamente: '请检查你的网络设置后重试',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: '搭配伴奏弹奏一首你最喜欢的曲子',
+    facaLoginParaAproveitarOMaximoDoAplicativo: '登录后可以使用更多功能'
+}
+
+export default strings;
