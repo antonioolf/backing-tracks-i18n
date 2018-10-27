e507f1e2c10ab59bf1fa1da7faddc6d791bb0301
Steve Riley <sriley@mmbs.ca>
2018-10-27 11:10:50 -0400

    Added fr-ca tranalation

---
+++ fr-ca.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Se connecter à l\'aide courriel.',
+    cadastro: 'Créer un compte',
+    senha: 'Mot de passe',
+    concluir: 's\'inscrire',
+    preenchaOFormularioCorretamente: 'Merci de remplir les champs correctement!',
+
+    // FBLogin
+    acessarComOFacebook: 'Se connecter à l\'aide de Facebook',
+    aguarde: 'Merci de patienter...',
+
+    // Home
+    musicasEmDestaque: 'Chansons en vedette',
+    artistasEmDestaque: 'Artisted en vedettes',
+    atividadeRecente: 'Activités récentes',
+    escutou: 'a écouté',
+    curtiu: 'a aimé',
+    aMusica: 'la chanson',
+
+    // Nav Bottom
+    home: 'Accueil',
+    buscar: 'Rechercher',
+    explorar: 'Explorer',
+    colecao: 'Collection',
+    perfil: 'Profil',
+
+    // Perfil
+    nome: 'Nom',
+    email: 'Adresse courriel',
+    sair: 'Se déconnectert',
+    instrumentoPrincipal: 'Instrument principal',
+    guitarra: 'Guitare',
+    baixo: 'Basse',
+    bateria: 'BAtterie',
+    voz: 'Voix',
+    violao: 'Guitare acoustique',
+    avalieNaGooglePlay: 'Noter l\'application',
+    editarPerfil: 'Modifier le profil',
+    faleConosco: 'Nous contacter',
+    comprarVersaoPremium: 'Acheter la version Premium',
+    removerAnuncios: 'Masquer les publicités (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'À propos de vous',
+    faleUmPoucoSobreVoce: 'Parlez-vous un peu de vous.',
+    localOndeMora: 'Localisation',
+    ondeVoceMora: 'Où habitez-vous?',
+    digiteSeuUsername: 'Votre nom d\'utilisateur. Exemple : @backingtracksapp.',
+    digiteSeuNumeroDeWhatsapp: 'Votre numéro WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Lien vers votre chaîne YouTube.',
+    canalNoYoutube: 'Chaîne YouTube',
+    esteDadoNaoPodeSerEditado: 'Cette information ne peut pas être modifiée! ',
+
+    // Editar
+    salvar: 'Enregistrer',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Veuillez préciser les détail du problème ou de votre question.',
+    enviar: 'Envoyer',
+
+    // Editar perfil
+    atualizeSeusDados: 'Modifier le profil',
+
+    // Coleção
+    musicasCurtidas: 'CHansons aimées',
+    
+    // Música
+    para: 'pour',
+    tonalidade: 'Slé',
+    outrasMusicas: 'Autres chansons',
+
+    // Offline
+    voceEstaOffline: 'Vous n\'êtes pas connectés!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Vérifier votre connection internet puis réessayez à nouveau.',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Jouez vos morceaux favoris avec accompagnement!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Connectez-vous pour profiter de toute l\'application'
+}
+
+export default strings;
