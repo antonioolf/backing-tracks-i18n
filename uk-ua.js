d5c59ec558d66c707c5b6b4ff481f714a7ec656f
igavelyuk <gavelyukworking@gmail.com>
2018-10-27 21:14:12 +0300

    Translated to Ukrainian, spell checked

---
+++ uk-ua.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Увійти за допомогою E-mail',
+    cadastro: 'Реєстрацiя',
+    senha: 'Пароль',
+    concluir: 'Завершити',
+    preenchaOFormularioCorretamente: 'Будь ласка заповніть поля форми корректно!',
+
+    // FBLogin
+    acessarComOFacebook: 'Увійти за допомогою Facebook',
+    aguarde: 'Зачекайте будь ласка...',
+
+    // Home
+    musicasEmDestaque: 'Рекомендовані Пісні',
+    artistasEmDestaque: 'Рекомендовані Артисти',
+    atividadeRecente: 'Нещодавна Активність',
+    escutou: 'слухав',
+    curtiu: 'сподобалось',
+    aMusica: 'пісня',
+
+    // Nav Bottom
+    home: 'Головна',
+    buscar: 'Пошук',
+    explorar: 'Дізнатися більше',
+    colecao: 'Колекція',
+    perfil: 'Профіль',
+
+    // Perfil
+    nome: 'Ім\'я',
+    email: 'E-mail',
+    sair: 'Вийти',
+    instrumentoPrincipal: 'Головний інструмент',
+    guitarra: 'Гітара',
+    baixo: 'Басс',
+    bateria: 'Барабан',
+    voz: 'Вокал',
+    violao: 'Акустична гітара',
+    avalieNaGooglePlay: 'Оцінити додаток',
+    editarPerfil: 'Редагувати профіль',
+    faleConosco: 'З\'эднатися з нами',
+    comprarVersaoPremium: 'Придбати Premium версію',
+    removerAnuncios: 'Видалити рекламу (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Інформація о вас',
+    faleUmPoucoSobreVoce: 'Розповісти нам дещо більше о вас',
+    localOndeMora: 'Місцеположення',
+    ondeVoceMora: 'Місце проживання?',
+    digiteSeuUsername: 'Введіть ім\'я пользователя. Приклад: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Введіть ваш WhatsApp номер',
+    linkParaSeuCanalNoYoutube: 'З\'єднай свій YouTube канал',
+    canalNoYoutube: 'YouTube канал',
+    esteDadoNaoPodeSerEditado: 'Ці дані не можуть бути модификовані!',
+
+    // Editar
+    salvar: 'Зберегти',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Будь ласка детально розповісти вашу проблему або задайте питання.',
+    enviar: 'Відправити',
+
+    // Editar perfil
+    atualizeSeusDados: 'Оновити профіль',
+
+    // Coleção
+    musicasCurtidas: 'Улюбленні Пісні',
+
+    // Música
+    para: 'для',
+    tonalidade: 'Тональність',
+    outrasMusicas: 'Інші пісні',
+
+    // Offline
+    voceEstaOffline: 'Ви offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Перевірте підключення до Інтернету та спробуйте ще раз.',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Грайте улюблені пісні під акомпанемент!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Увійдіть, щоб максимально насолодитися додатком'
+}
+
+export default strings;
