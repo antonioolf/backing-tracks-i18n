0265619c50687a4db583c708257e0cf168d3363c
Jovan Ferryal E. F <jovanzers@gmail.com>
2018-10-29 02:04:49 +0700

    Add Indonesian translation

---
+++ App texts/id-id.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Masuk dengan E-mail',
+    cadastro: 'Daftar',
+    senha: 'Kata Sandi',
+    concluir: 'Selesai',
+    preenchaOFormularioCorretamente: 'Silakan isi formulir dengan benar!',
+
+    // FBLogin
+    acessarComOFacebook: 'Masuk dengan Facebook',
+    aguarde: 'Tunggu sebentar...',
+
+    // Home
+    musicasEmDestaque: 'Lagu Pilihan',
+    artistasEmDestaque: 'Artis Pilihan',
+    atividadeRecente: 'Aktivitas Terbaru',
+    escutou: 'mendengarkan',
+    curtiu: 'menyukai',
+    aMusica: 'lagu',
+
+    // Nav Bottom
+    home: 'Beranda',
+    buscar: 'Cari',
+    explorar: 'Jelajahi',
+    colecao: 'Koleksi',
+    perfil: 'Profil',
+
+    // Perfil
+    nome: 'Nama',
+    email: 'E-mail',
+    sair: 'Keluar',
+    instrumentoPrincipal: 'Instrumen utama',
+    guitarra: 'Gitar',
+    baixo: 'Bass',
+    bateria: 'Drum',
+    voz: 'Vokal',
+    violao: 'Gitar akustik',
+    avalieNaGooglePlay: 'Beri Nilai Aplikasi',
+    editarPerfil: 'Ubah profil',
+    faleConosco: 'Hubungi kami',
+    comprarVersaoPremium: 'Beli versi Premium',
+    removerAnuncios: 'Hapus iklan (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Tentang kamu',
+    faleUmPoucoSobreVoce: 'Ceritakan sedikit tentang dirimu',
+    localOndeMora: 'Lokasi',
+    ondeVoceMora: 'Dimana kamu tinggal?',
+    digiteSeuUsername: 'Masukkan nama pengguna Anda. Contoh: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Masukkan nomor WhatsApp Anda',
+    linkParaSeuCanalNoYoutube: 'Tautan ke saluran YouTube Anda',
+    canalNoYoutube: 'Saluran YouTube',
+    esteDadoNaoPodeSerEditado: 'Data ini tidak dapat diedit!',
+
+    // Editar
+    salvar: 'Simpan',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Tolong jelaskan secara rinci masalah atau pertanyaan Anda.',
+    enviar: 'Kirim',
+
+    // Editar perfil
+    atualizeSeusDados: 'Perbarui info Anda',
+
+    // Coleção
+    musicasCurtidas: 'Lagu yang Disukai',
+    
+    // Música
+    para: 'untuk',
+    tonalidade: 'Kunci',
+    outrasMusicas: 'Lagu lainnya',
+
+    // Offline
+    voceEstaOffline: 'Anda sedang offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Periksa koneksi internet Anda dan coba lagi',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Mainkan lagu favorit Anda dengan iringan!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Masuk untuk mendapatkan hasil maksimal dari aplikasi Anda'
+}
+
+export default strings;
