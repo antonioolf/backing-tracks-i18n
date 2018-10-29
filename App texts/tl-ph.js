99116605573753d7f902d9eb80304554b9f6100e
Aria Fong <aria.nimue@gmail.com>
2018-10-29 13:29:36 +0800

    added en-PH and edited tl-PH App texts

--- App texts/tl-ph.js
+++ App texts/tl-ph.js
@@ -1,83 +1,87 @@
 let strings = {
+  // Cadastro
+  entrarComEmail: "Mag-sign-in gamit ang E-mail",
+  cadastro: "Magpatala",
+  senha: "Password",
+  concluir: "Tapos na",
+  preenchaOFormularioCorretamente: "Paki-ayos ang pagsagot sa form.",
 
-    // Cadastro
-    entrarComEmail: 'Mag-sign-in gamit ang E-mail',
-    cadastro: 'Magpatala',
-    senha: 'Password',
-    concluir: 'Tapos na',
-    preenchaOFormularioCorretamente: 'Paki-ayos po ang pagsagot sa form.',
+  // FBLogin
+  acessarComOFacebook: "Mag-sign-in gamit ang Facebook",
+  aguarde: "Sandali lamang...",
 
-    // FBLogin
-    acessarComOFacebook: 'Mag-sign-in sa Facebook',
-    aguarde: 'Maghintay lamang ng sandali...',
+  // Home
+  musicasEmDestaque: "Mga Awiting itinampok",
+  artistasEmDestaque: "Mga musikerong itinampok",
+  atividadeRecente: "Huling aktibidad",
+  escutou: "mga pinakinggan",
+  curtiu: "liked",
+  aMusica: "ang kanta",
 
-    // Home
-    musicasEmDestaque: 'Mga Awiting itinampok',
-    artistasEmDestaque: 'Mga musikerong itinampok',
-    atividadeRecente: 'Mga huling aktibidad',
-    escutou: 'mga pinakinggan',
-    curtiu: 'liked',
-    aMusica: 'ang kanta',
+  // Nav Bottom
+  home: "Home",
+  buscar: "Mag-search",
+  explorar: "Mag-explore",
+  colecao: "Koleksyon",
+  perfil: "Profile",
 
-    // Nav Bottom
-    home: 'Home',
-    buscar: 'Maghanap',
-    explorar: 'Maghanap-hanap',
-    colecao: 'Koleksyon',
-    perfil: 'Profile',
+  // Perfil
+  nome: "Pangalan",
+  email: "E-mail",
+  sair: "Mag-logout",
+  instrumentoPrincipal: "Pangunahing instrumento",
+  guitarra: "Gitara",
+  baixo: "Bass",
+  bateria: "Drums",
+  voz: "Boses/Bokal",
+  violao: "Gitara",
+  avalieNaGooglePlay: "I-rate ang App",
+  editarPerfil: "Baguhin ang iyong profile",
+  faleConosco: "Makipag-ugnayan sa amin",
+  comprarVersaoPremium: "Bilhin ang Premium version",
+  removerAnuncios: "Tanggalin ang mga ads (Pro)",
 
-    // Perfil
-    nome: 'Pangalan',
-    email: 'E-mail',
-    sair: 'Mag-logout',
-    instrumentoPrincipal: 'Pangunahing instrumento',
-    guitarra: 'bajo sexto',
-    baixo: 'Bass',
-    bateria: 'Drums',
-    voz: 'Boses',
-    violao: 'Gitara',
-    avalieNaGooglePlay: 'I-rate ang App',
-    editarPerfil: 'Baguhin ang profile',
-    faleConosco: 'Makipag-ugnayan sa amin',
-    comprarVersaoPremium: 'Bilhin ang Premium version',
-    removerAnuncios: 'Tanggalin ang mga ads (Pro)',
+  //Meus Dados
+  sobreVoce: "Mga personal na impormasyon",
+  faleUmPoucoSobreVoce: "Magbigay ng ilang impormasyon tungkol sa 'yo.",
+  localOndeMora: "Lokasyon",
+  ondeVoceMora: "Saan ka nakatira?",
+  digiteSeuUsername:
+    "Ilagay ang nais na username. Halimbawa: @backingtracksapp",
+  digiteSeuNumeroDeWhatsapp: "Ilagay ang iyong WhatsApp number",
+  linkParaSeuCanalNoYoutube: "Link ng iyong YouTube channel",
+  canalNoYoutube: "YouTube channel",
+  esteDadoNaoPodeSerEditado: "Hindi ito maaaring baguhin!",
 
-    //Meus Dados
-    sobreVoce: 'Mga personal na impormasyon',
-    faleUmPoucoSobreVoce: "Magbigay ng ilang impormasyon tungkol sa 'yo.",
-    localOndeMora: 'Lokasyon',
-    ondeVoceMora: 'Pook ng tirahan?',
-    digiteSeuUsername: 'Ilagay ang nais mong username. Halimbawa: @backingtracksapp',
-    digiteSeuNumeroDeWhatsapp: 'Ilagay ang iyong WhatsApp number',
-    linkParaSeuCanalNoYoutube: 'Link ng YouTube channel mo',
-    canalNoYoutube: 'YouTube channel',
-    esteDadoNaoPodeSerEditado: 'Hindi ito maaaring baguhin!',
+  // Editar
+  salvar: "i-save",
 
-    // Editar
-    salvar: 'i-save',
+  // Fale conosco
+  porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida:
+    "I-type ng mabuti ang iyong problema o tanong.",
+  enviar: "Ipadala",
 
-    // Fale conosco
-    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Paki-pahayag ng detalyado ang inyong problema o tanong.',
-    enviar: 'Ipadala',
+  // Editar perfil
+  atualizeSeusDados: "I-update ang inyong impormasyon",
 
-    // Editar perfil
-    atualizeSeusDados: 'I-update ang inyong impormasyon',
+  // Coleção
+  musicasCurtidas: "Ni-like na kanta",
 
-    // Coleção
-    musicasCurtidas: 'Mga Awit ng Pag-ibig',
-    
-    // Música
-    para: 'para',
-    tonalidade: 'Key',
-    outrasMusicas: 'Iba pang mga kanta',
+  // Música
+  para: "para",
+  tonalidade: "Key",
+  outrasMusicas: "Iba pang mga kanta",
 
-    // Offline
-    voceEstaOffline: 'Ikaw hindi nakakabit sa Internet!',
-    verifiqueASuaConexaoComAInternetETenteNovamente: 'Ayusin muna ang inyong Internet bago subukan ulit.',
+  // Offline
+  voceEstaOffline: "Wala kang internet connection",
+  verifiqueASuaConexaoComAInternetETenteNovamente:
+    "Ayusin ang internet connection at subukan muli",
 
-    // Login
-    toqueSuasMusicasFavoritasComAcompanhamento: 'Patugtugin ang mga paboritong kanta nang may saliw.',
-    facaLoginParaAproveitarOMaximoDoAplicativo: 'Mag-sign-in para mas mapakinabangan ang app'
-}
+  // Login
+  toqueSuasMusicasFavoritasComAcompanhamento:
+    "Patugtugin ang mga paboritong kanta nang may accompaniment",
+  facaLoginParaAproveitarOMaximoDoAplicativo:
+    "Mag-sign-in para mas mapakinabangan ang app"
+};
 
 export default strings;
