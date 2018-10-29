86eacaccaa32c11c7eceeba7476253a982d024be
phucduong86 <phuc.duongdinh@gmail.com>
2018-10-28 22:32:40 -0400

    complete vietnamese translation

---
+++ vi-vn.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Đăng nhập bằng E-mail',
+    cadastro: 'Đăng ký',
+    senha: 'Mật khẩu',
+    concluir: 'Hoàn thành',
+    preenchaOFormularioCorretamente: 'Vui lòng điền chính xác vào biểu mẫu!',
+
+    // FBLogin
+    acessarComOFacebook: 'Đăng nhập bằng Facebook',
+    aguarde: 'Đợi một chút...',
+
+    // Home
+    musicasEmDestaque: 'Bài hát',
+    artistasEmDestaque: 'Nghệ sĩ',
+    atividadeRecente: 'Hoạt động gần đây',
+    escutou: 'Đã nghe',
+    curtiu: 'thích',
+    aMusica: 'bài hát',
+
+    // Nav Bottom
+    home: 'Trang chủ',
+    buscar: 'Tìm kiếm',
+    explorar: 'Khám phá',
+    colecao: 'Bộ sưu tập',
+    perfil: 'Hồ sơ',
+
+    // Perfil
+    nome: 'Tên',
+    email: 'E-mail',
+    sair: 'Đăng xuất',
+    instrumentoPrincipal: 'Nhạc cụ chính',
+    guitarra: 'Đàn Ghi Ta',
+    baixo: 'Đàn Guitar Bass',
+    bateria: 'Trống',
+    voz: 'Ca sĩ ',
+    violao: 'Ghi Ta Acoustic',
+    avalieNaGooglePlay: 'Đánh giá ứng dụng',
+    editarPerfil: 'Chỉnh sửa hồ sơ',
+    faleConosco: 'Liên hệ chúng tôi',
+    comprarVersaoPremium: 'Mua phiên bản đặc biệt',
+    removerAnuncios: 'Loại bỏ các quảng cáo (Chuyên nghiệp)',
+
+    //Meus Dados
+    sobreVoce: 'Về bạn',
+    faleUmPoucoSobreVoce: 'Nói với chúng tôi một chút về bạn',
+    localOndeMora: 'Vị trí',
+    ondeVoceMora: 'Bạn sống ở đâu?',
+    digiteSeuUsername: 'Nhập tên người dùng của bạn. Thí dụ: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Nhập số WhatsApp của bạn',
+    linkParaSeuCanalNoYoutube: 'Liên kết tới kênh YouTube của bạn',
+    canalNoYoutube: 'Kênh YouTube',
+    esteDadoNaoPodeSerEditado: 'Không thể chỉnh sửa dữ liệu này!',
+
+    // Editar
+    salvar: 'Lưu trữ',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Vui lòng giải thích chi tiết vấn đề hoặc câu hỏi của bạn.',
+    enviar: 'Gửi',
+
+    // Editar perfil
+    atualizeSeusDados: 'Cập nhật thông tin của bạn',
+
+    // Coleção
+    musicasCurtidas: 'Bài hát yêu thích',
+    
+    // Música
+    para: 'for',
+    tonalidade: 'Key',
+    outrasMusicas: 'Các bài hát khác',
+
+    // Offline
+    voceEstaOffline: 'You are offline!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Kiểm tra kết nối internet của bạn và thử lại',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Chơi các bài hát yêu thích của bạn với phần đệm!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Đăng nhập để tận dụng tối đa ứng dụng của bạn!'
+}
+
+export default strings;
