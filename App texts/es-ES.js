39d19558ef24943b9c3e87defcf6e5129d0aeb6e
Sara <sara.her.su@gmail.com>
2018-10-30 09:06:48 +0000

    Add es-ES translation for App texts

---
+++ App texts/es-ES.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Acceder con e-mail',
+    cadastro: 'Registrarse',
+    senha: 'Contraseña',
+    concluir: 'Finalizar',
+    preenchaOFormularioCorretamente: '¡Rellena el formulario correctamente, por favor!',
+
+    // FBLogin
+    acessarComOFacebook: 'Acceder con Facebook',
+    aguarde: 'Espera un momento...',
+
+    // Home
+    musicasEmDestaque: 'Canciones destacadas',
+    artistasEmDestaque: 'Artistas destacados',
+    atividadeRecente: 'Actividad reciente',
+    escutou: 'has escuchado',
+    curtiu: 'te ha gustado',
+    aMusica: 'la canción',
+
+    // Nav Bottom
+    home: 'Inicio',
+    buscar: 'Buscar',
+    explorar: 'Explorar',
+    colecao: 'Colección',
+    perfil: 'Perfil',
+
+    // Perfil
+    nome: 'Nombre',
+    email: 'E-mail',
+    sair: 'Desconectarse',
+    instrumentoPrincipal: 'Instrumento principal',
+    guitarra: 'Guitarra',
+    baixo: 'Bajo',
+    bateria: 'Percusión',
+    voz: 'Voz',
+    violao: 'Guitarra acústica',
+    avalieNaGooglePlay: 'Valora la aplicación',
+    editarPerfil: 'Editar perfil',
+    faleConosco: 'Contáctanos',
+    comprarVersaoPremium: 'Compra la edición Premium',
+    removerAnuncios: 'Quita los anuncios (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Sobre ti',
+    faleUmPoucoSobreVoce: 'Cuéntanos un poco sobre ti',
+    localOndeMora: 'Ubicación',
+    ondeVoceMora: '¿Dónde vives?',
+    digiteSeuUsername: 'Escribe tu nombre de usuario. Ejemplo: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Escribe tu número de WhatsApp',
+    linkParaSeuCanalNoYoutube: 'Pon un enlace a tu canal de YouTube',
+    canalNoYoutube: 'Canal de YouTube',
+    esteDadoNaoPodeSerEditado: '¡No puedes modificar este dato!',
+
+    // Editar
+    salvar: 'Guardar',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Explica en detalle tu duda o problema, por favor.',
+    enviar: 'Enviar',
+
+    // Editar perfil
+    atualizeSeusDados: 'Actualiza tus datos',
+
+    // Coleção
+    musicasCurtidas: 'Canciones favoritas',
+
+    // Música
+    para: 'para',
+    tonalidade: 'Tono',
+    outrasMusicas: 'Otras canciones',
+
+    // Offline
+    voceEstaOffline: '¡Estás desconectado!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Comprueba tu conexión a internet e inténtalo de nuevo',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: '¡Interpreta tus canciones favoritas con acompañamiento!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Identifícate para aprovechar al máximo la aplicación'
+}
+
+export default strings;
