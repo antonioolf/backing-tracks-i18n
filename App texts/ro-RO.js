f1a4f9c5d9153660098e2846c59dca5cfe333137
Alexandru Buhai <alexandru.buhai@sectorlabs.ro>
2019-10-03 22:01:38 +0300

    Add Romanian translation for description and js
    Thanks for letting me contribute.

---
+++ App texts/ro-RO.js
@@ -0,0 +1,84 @@
+let strings = {
+
+  // Cadastro
+  entrarComEmail: 'Înregistrează-te cu E-mail',
+  cadastro: 'Înregistrează-te',
+  senha: 'Parola',
+  concluir: 'Finalizare',
+  preenchaOFormularioCorretamente: 'Te rugam compeltează corect!',
+
+  // FBLogin
+  acessarComOFacebook: 'Înregistrează-te cu Facebook',
+  aguarde: 'Asteapta un moment...',
+
+  // Home
+  musicasEmDestaque: 'Piese recomandate',
+  artistasEmDestaque: 'Artiști recomandați',
+  atividadeRecente: 'Activitate recenta',
+  escutou: 'ați ascultat',
+  curtiu: 'apreciat',
+  aMusica: 'piesa',
+
+  // Nav Bottom
+  home: 'Acasă',
+  buscar: 'Caută',
+  explorar: 'Explorează',
+  colecao: 'Colecție',
+  perfil: 'Profil',
+
+  // Perfil
+  nome: 'Nume',
+  email: 'E-mail',
+  sair: 'Deconectați-vă',
+  instrumentoPrincipal: 'Instrument principal',
+  guitarra: 'Chitara',
+  baixo: 'Chitară bas',
+  bateria: 'Tobe',
+  voz: 'Voce',
+  violao: 'Chitară acustica',
+  avalieNaGooglePlay: 'Dă nota aplicației',
+  editarPerfil: 'Editează profil',
+  faleConosco: 'Contactează-ne',
+  comprarVersaoPremium: 'Cumpără versiunea premium',
+  removerAnuncios: 'Scapă de publicitate (Pro)',
+
+  //Meus Dados
+  sobreVoce: 'Despre tine',
+  faleUmPoucoSobreVoce: 'Spune-ne ceva despre tine',
+  localOndeMora: 'Locație',
+  ondeVoceMora: 'De unde ești?',
+  digiteSeuUsername: 'Adaugă un nume de utilizator. Exemplu: @backingtracksapp',
+  digiteSeuNumeroDeWhatsapp: 'Adaugă numarul de WhatsApp',
+  linkParaSeuCanalNoYoutube: 'Adaugă canalul de YouTube',
+  canalNoYoutube: 'Canal de YouTube',
+  esteDadoNaoPodeSerEditado: 'Acest câmp nu poate fi editat!',
+
+  // Editar
+  salvar: 'Salvează',
+
+  // Fale conosco
+  porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Explică in detaliu problema sau întrebarea.',
+  enviar: 'Trimite',
+
+  // Editar perfil
+  atualizeSeusDados: 'Actualizați datele dumneavoastră',
+
+  // Coleção
+  musicasCurtidas: 'Piese apreciate',
+  
+  // Música
+  para: 'pentru',
+  tonalidade: 'Cheie',
+  outrasMusicas: 'Alte piese',
+
+  // Offline
+  voceEstaOffline: 'Sunteți deconectat!',
+  verifiqueASuaConexaoComAInternetETenteNovamente: 'Verificați conexiunea la internet și încercați din nou',
+
+  // Login
+  toqueSuasMusicasFavoritasComAcompanhamento: 'Redă melodiile preferate cu acompaniament!',
+  facaLoginParaAproveitarOMaximoDoAplicativo: 'Conectați-vă pentru a beneficia la maxim de aplicație'
+}
+
+export default strings;
+
