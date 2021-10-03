498fc176815b67f35c5365609777cb7d0f852d86
Theo Dowling <thdowling@gmail.com>
2021-10-03 21:17:12 +0200

    Adding Afrikaans translation

---
+++ App texts/af.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'Teken aan met epos.',
+    cadastro: 'Registreer',
+    senha: 'Wagwoord',
+    concluir: 'Voltooi',
+    preenchaOFormularioCorretamente: 'Vul asseblief die vorm korrek!',
+
+    // FBLogin
+    acessarComOFacebook: 'Teken aan met Facebook.',
+    aguarde: 'Wag \'n oomblik...',
+
+    // Home
+    musicasEmDestaque: 'Voorgestelde liedjies',
+    artistasEmDestaque: 'Voorgestelde kunstenaars',
+    atividadeRecente: 'Onlangse aktiwiteite',
+    escutou: 'geluister',
+    curtiu: 'hou van',
+    aMusica: 'die liedjie',
+
+    // Nav Bottom
+    home: 'Tuis',
+    buscar: 'Soek',
+    explorar: 'Snuffel',
+    colecao: 'versameling',
+    perfil: 'Profiel',
+
+    // Perfil
+    nome: 'Naam',
+    email: 'Epos',
+    sair: 'Teken af',
+    instrumentoPrincipal: 'Hoof instrument',
+    guitarra: 'Kitaar',
+    baixo: 'Bass',
+    bateria: 'Tromme',
+    voz: 'Sang',
+    violao: 'Akoestiese guitar',
+    avalieNaGooglePlay: 'Beoordeel app',
+    editarPerfil: 'Wysig profiel',
+    faleConosco: 'Kontak ons',
+    comprarVersaoPremium: 'Koop premium weergawe',
+    removerAnuncios: 'Verwyder ads (Pro).',
+
+    //Meus Dados
+    sobreVoce: 'Oor you',
+    faleUmPoucoSobreVoce: 'Verduidelik ons meer van jouself.',
+    localOndeMora: 'Area',
+    ondeVoceMora: 'Waar woon jy?',
+    digiteSeuUsername: 'Tik jou gebruikersnaam. bv. @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'Wat is jou WhatsApp nommer.',
+    linkParaSeuCanalNoYoutube: 'Skakel tot jou Youtube kanaal.',
+    canalNoYoutube: 'Youtube kanaal',
+    esteDadoNaoPodeSerEditado: 'Die data kan nie verander word nie!',
+
+    // Editar
+    salvar: 'Stoor',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Verduidelik asb in detail jou probleem.',
+    enviar: 'Stuur',
+
+    // Editar perfil
+    atualizeSeusDados: 'Dateer jou inligting op.',
+
+    // Coleção
+    musicasCurtidas: 'Gunstelling liedjies',
+    
+    // Música
+    para: 'vir',
+    tonalidade: 'Skakel',
+    outrasMusicas: 'Ander liedere',
+
+    // Offline
+    voceEstaOffline: 'Jy is van die lyn af!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Kyk na jou internet konneksie en probeer weer.',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Speel jou gunsteling liedjies met begeleiding!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Teken aan om die meeste uit die app te kry.'
+}
+
+export default strings;
