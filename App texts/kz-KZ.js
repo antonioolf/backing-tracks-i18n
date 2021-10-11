1be724d119e9728c56f8aa25e7a7c8cbf2ff3e5a
Anatoliy Serputov <aserputov@myseneca.ca>
2021-10-11 01:51:32 -0400

    KZ-kz(Kazakh) language added

---
+++ App texts/kz-KZ.js
@@ -0,0 +1,83 @@
+let strings = {
+
+    // Cadastro
+    entrarComEmail: 'E-mail арқылы кіріңіз',
+    cadastro: 'Дәл қазір тіркеліңіз',
+    senha: 'Құпия сөз',
+    concluir: 'Аяқтау!',
+    preenchaOFormularioCorretamente: 'Өтініш форманы дұрыс толтырыңыз!',
+
+    // FBLogin
+    acessarComOFacebook: 'Facebook арқылы кіріңіз',
+    aguarde: 'Бір минут күте тұрыңыз ...',
+
+    // Home
+    musicasEmDestaque: 'Таңдалған әндер',
+    artistasEmDestaque: 'Таңдаулы әртістер',
+    atividadeRecente: 'Соңғы әрекет',
+    escutou: 'тыңдады',
+    curtiu: 'ұнады',
+    aMusica: 'өлең',
+
+    // Nav Bottom
+    home: 'Yй',
+    buscar: 'Іздеу',
+    explorar: 'Зерттеу',
+    colecao: 'Коллекция',
+    perfil: 'Парақшам',
+
+    // Perfil
+    nome: 'Аты',
+    email: 'E-mail',
+    sair: 'Шығу',
+    instrumentoPrincipal: 'Негізгі құрал',
+    guitarra: 'Гитара',
+    baixo: 'Бас',
+    bateria: 'Барабандар',
+    voz: 'Вокал',
+    violao: 'Акустикалық гитара',
+    avalieNaGooglePlay: 'Қолданбаны бағалау',
+    editarPerfil: 'Парақшам өзгерту',
+    faleConosco: 'Бізбен байланысыңыз',
+    comprarVersaoPremium: 'Премиум нұсқасын сатып алыңыз',
+    removerAnuncios: 'Жарнамаларды жою (Pro)',
+
+    //Meus Dados
+    sobreVoce: 'Сен туралы',
+    faleUmPoucoSobreVoce: 'Өзіңіз туралы қысқаша айтып өтсеңіз',
+    localOndeMora: 'Орналасуы',
+    ondeVoceMora: 'Сіз қайда тұрасыз?',
+    digiteSeuUsername: 'Пайдаланушы атыңызды енгізіңіз. Мысалы: @backingtracksapp',
+    digiteSeuNumeroDeWhatsapp: 'WhatsApp нөміріңізді енгізіңіз',
+    linkParaSeuCanalNoYoutube: 'YouTube арнасына сілтеме',
+    canalNoYoutube: 'YouTube арнасы',
+    esteDadoNaoPodeSerEditado: 'Бұл деректерді өзгерту мүмкін емес!',
+
+    // Editar
+    salvar: 'Сақтау',
+
+    // Fale conosco
+    porFavorExpliqueDetalhadamenteOSeuProblemaOuDuvida: 'Мәселеңізді немесе сұрағыңызды егжей -тегжейлі сипаттаңыз.',
+    enviar: 'Жіберу',
+
+    // Editar perfil
+    atualizeSeusDados: 'Ақпаратыңызды жаңартыңыз',
+
+    // Coleção
+    musicasCurtidas: 'Ұнаған әндер',
+    
+    // Música
+    para: 'үшін',
+    tonalidade: 'Кілт',
+    outrasMusicas: 'Басқа Әндер',
+
+    // Offline
+    voceEstaOffline: 'Сіз желіде емессіз!',
+    verifiqueASuaConexaoComAInternetETenteNovamente: 'Интернет байланысын тексеріп, қайталап көріңіз',
+
+    // Login
+    toqueSuasMusicasFavoritasComAcompanhamento: 'Өзіңіздің сүйікті әндеріңізді сүйемелдеуімен ойнаңыз!',
+    facaLoginParaAproveitarOMaximoDoAplicativo: 'Қолданбаның барлық мүмкіндіктерін алу үшін жүйеге кіріңіз'
+}
+
+export default strings;
